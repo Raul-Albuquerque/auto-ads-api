1662228cfc2498bdbcfedf1c480a4cf6735153ab
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2025-04-17 02:59:41 -0300

    fix: appscheduler bug

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import asyncio
 from routes import utmify_router, report_router,health_router,test_router, vturb_router, write_ads_escalados_router
 
 app = FastAPI()
@@ -18,3 +21,32 @@ app.include_router(health_router)
 app.include_router(test_router)
 app.include_router(vturb_router)
 app.include_router(write_ads_escalados_router)
+
+from services.api_request import call_until_success
+
+endpoints = [
+    "https://automacao-nkh9m.ondigitalocean.app/utmify/campaigns/escalados/today",
+    "https://automacao-nkh9m.ondigitalocean.app/utmify/campaigns/escalados/sales/today",
+    "https://automacao-nkh9m.ondigitalocean.app/test/ads/escalados/daily"
+]
+
+# Função que será chamada pelo agendador
+async def run_scheduled_job():
+    print("Executando job automático...")
+    resultados = []
+    for endpoint in endpoints:
+        resultado = await call_until_success(endpoint)
+        resultados.append(resultado)
+    print("Resultado do job automático:", resultados)
+
+# Wrapper porque APScheduler só aceita funções síncronas no `add_job`
+def schedule_job():
+    asyncio.create_task(run_scheduled_job())
+
+# Configura o agendador
+scheduler = AsyncIOScheduler()
+
+# Roda a cada 2h das 8h até 00h
+scheduler.add_job(schedule_job, CronTrigger(hour=3, minute=02))
+
+scheduler.start()
